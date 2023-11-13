4a8e212c73f05d3a348ce5936ea75ef5ab7a3022
DESKTOP-FEDOR <pichuginf2016@mail.ru>
2023-11-13 23:21:18 +0500

    added a test

--- GeometryTests/TriangleShould.cs
+++ GeometryTests/TriangleShould.cs
@@ -32,14 +32,20 @@ public class TriangleShould
     public void TriangleSquare()
     {
         var triangle = new Triangle(3, 3, 2);
-        var expected = 0.5 * 2 * Math.Sqrt(8); 
+        var expected = 0.5 * 2 * Math.Sqrt(8);
         Assert.That(triangle.Square, Is.EqualTo(expected).Within(1e-6));
     }
 
     [Test]
     public void TriangleIsRectangular()
     {
-        var triangle = new Triangle(3, 4, 5);
-        Assert.That(triangle.IsRectangular, Is.EqualTo(true));
-    } 
+        var triangle1 = new Triangle(3, 4, 5);
+        var triangle2 = new Triangle(3, 3, 2);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(triangle1.IsRectangular, Is.EqualTo(true));
+            Assert.That(triangle2.IsRectangular, Is.EqualTo(false));
+        });
+    }
 }
